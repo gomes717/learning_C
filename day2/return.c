763dd4a90cbed764606996263de9a17e7cbede31
Gomes <barbozaguilherme717@gmail.com>
2022-07-11 22:43:35 -0300

    day2 added return.c

---
+++ day2/return.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+void *threadDice()
+{
+	int dado = (rand()%6) +1;
+    int* result = (int*) malloc(sizeof(int));
+    *result = dado;
+    printf("Address of returned in thread: %p\n", result);
+    return (void*) result;
+}
+
+int main()
+{
+	pthread_t t1;
+    int* returned;
+
+	if(pthread_create(&t1, NULL, threadDice, NULL))
+	{
+		printf("thread 1 not created\n");
+		exit(0);
+	}
+
+	pthread_join(t1, (void**) &returned);
+
+    printf("Address of returned in main: %p\n", returned);
+    printf("result of threadDice: %d\n", *returned);
+    free(returned);
+
+	return 0;
+}
