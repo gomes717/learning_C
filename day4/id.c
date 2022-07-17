058d3171e0a24e106cb6badaae6d64483ef1960c
Gomes <barbozaguilherme717@gmail.com>
2022-07-17 04:06:05 -0300

    day4 added id

---
+++ day4/id.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+
+int main()
+{
+    pid_t pid = fork();
+
+    if(pid == 0)
+    {
+        sleep(1);
+    }
+
+    printf("Current ID: %d\n", getpid());
+    printf("Parent ID: %d\n", getppid());
+
+    int res = wait(NULL);
+    if(res == -1)
+    {
+        printf("Process without child\n");
+    }
+    else
+    {
+        printf("child with id %d has finished\n", res);
+    }
+    return 0;
+}
