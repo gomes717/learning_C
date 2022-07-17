011f4538da988dc0994a2f96b52f49895ae7a6ff
Gomes <barbozaguilherme717@gmail.com>
2022-07-17 03:42:57 -0300

    day4 added processes2

---
+++ day4/processes2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+
+int main()
+{
+    pid_t pid = fork();
+    int n;
+    if(pid == 0)
+    {
+        n = 1;
+    }
+    else
+    {
+        n = 6;
+        wait();
+    }
+
+
+    for(int i = n; i < n+5; i++)
+    {
+        printf("%d ", i);
+        fflush(stdout);
+    }
+    return 0;
+}
